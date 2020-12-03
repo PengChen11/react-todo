a52963befcdffaa9dda846ba0ecd73b13d9c6100
Peng Chen <pchen0816@outlook.com>
2020-12-02 16:06:14 -0800

    bug fix

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import App from '../App.js';
+import App from '../app.js';
 import React from 'react';
 
 test.skip('renders to do list manager', () => {
