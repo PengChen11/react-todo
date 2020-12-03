10af0c8e0249d2158a8da95b3960dd549a3a451d
Peng Chen <pchen0816@outlook.com>
2020-12-02 16:51:07 -0800

    login shows loading bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,22 @@
-import {React} from 'react';
+import React, {useContext} from 'react';
 import {Navbar} from 'react-bootstrap';
 import Login from '../context/auth/login.js';
+import ReactLoading from 'react-loading';
+import {LoginContext} from '../context/auth/context.js';
+import {When} from 'react-if';
 
 export default function Header (){
+  const {isLoading} = useContext(LoginContext);
+
   return(
     <>
       <Navbar bg="primary" variant="dark" className="justify-content-between">
         <Navbar.Brand href="#home" className="d-inline">Home</Navbar.Brand>
         <Login />
       </Navbar>
+      <When condition = {isLoading}>
+        <ReactLoading type={'bars'} color={'grey'} width={150} className='m-auto' />
+      </When>
     </>
   );
 }
